eb617ac3557fac0a055531c48597a72699f2018b
BidkarSolisPerez <bidkr.solis@gmail.com>
2018-04-14 19:22:19 -0600

    PHP practice
    PHP practice lab4

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <style>
+    #left-panel{
+      background-color: red;
+    }
+    #rigth-panel{
+      background-color: blue;
+    }
+  </style>
+
+</head>
+<body>
+
+  <div id="left-panel">
+
+  </div>
+  <div id="rigth-panel">
+
+  </div>
+  <?php
+    $mensaje = "Hola mundo";
+    echo "<h1>$mensaje</h1>"
+   ?>
+
+</body>
+</html>
